636ee6d122d2defcda7ace44c7180ad4399156a1
Sagar <sagar@localhost.localdomain>
2017-07-14 14:54:09 +0530

    Second Commit

---
+++ function.py
@@ -0,0 +1,13 @@
+def repeat(s, exclaim):
+    """
+    Returns the string 's' repeated 3 times.
+    If exclaim is true, add exclamation marks.
+    """
+
+    result = s + s + s # can also use "s * 3" which is faster (Why?)
+    if exclaim:
+        result = result + '!!!'
+    return result
+
+print repeat('Sagar',True)
+print repeat('sagar Aivale',False)
